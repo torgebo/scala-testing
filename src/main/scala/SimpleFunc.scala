6c2a2c525119f87356d74cd39c964e38d94294fc
Torgeir Boerresen <torgeir.borresen@sigmagroup.no>
2016-02-03 08:43:41 +0100

    Removed some old comments

--- src/main/scala/SimpleFunc.scala
+++ src/main/scala/SimpleFunc.scala
@@ -2,9 +2,6 @@
 object SizeHandlers {
 
   def bytesRequired(fields: Int, fieldsPerByte: Int): Int = {
-    // Rounds up correctly. Potiential overflow though
-    //(fields+(fieldsPerByte-1)) / fieldsPerByte //overflowed version
-    // if f = xB+y then we want xB plus one extra byte if needed
     val x = fields/fieldsPerByte
     val extra = (fields-(x-1)*fieldsPerByte -1)/fieldsPerByte
     x + extra
